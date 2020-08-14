d2e64a5a2825c72820ba6427acf4bc3f0789f13b
smarten <smarten.developer@gmail.com>
2020-08-13 20:57:58 -0700

    inital skeleton

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,34 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  const $schedule = $(".schedule");
+  const dayHours = [9, 10, 11, 12, 1, 2, 3, 4, 5];
 
+  renderPage();
+  function renderPage() {
+    for (let i = 0; i < dayHours.length; i++) {
+      const $row = $("<div>");
+      const $task = $("<div>");
+      const $time = $("<div>");
+      const $updateContent = $("<button>");
+
+      $row.addClass("row align-items-center");
+
+      $time.text("12PM").addClass("col-2 align-items-center").css({
+        border: "2px solid black",
+        padding: "27px",
+      });
+
+      $task
+        .text(" ")
+        .css({
+          padding: "40px",
+          "background-color": "gray",
+          border: "2px solid black",
+        })
+        .addClass("col-8");
+
+      $updateContent.addClass("col-2 d-inline-block").css("height", "84px");
+
+      $schedule.append($row.append($time, $task, $updateContent));
+    }
+  }
 });
