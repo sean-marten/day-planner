9d18344f7e351179f1743b5a5fde63948fcbc23e
smarten <smarten.developer@gmail.com>
2020-08-13 23:19:11 -0700

    final design

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
+  // Declare variables
   const $schedule = $(".schedule");
   const dayHours = [9, 10, 11, 12, 1, 2, 3, 4, 5];
-  let tasks = []
+  let tasks = [];
 
   // Initialize the planner by checking if there is locally stored data
   function init() {
@@ -10,33 +11,47 @@ $(document).ready(function () {
       tasks = storedTasks;
     }
   }
+
+  // Call methods to populate webpage
   init();
   renderPage();
 
-
+  // Method to dynamically add schedule rows
   function renderPage() {
     for (let i = 0; i < dayHours.length; i++) {
       const $row = $("<div>");
       const $task = $("<input>");
       const $time = $("<div>");
       const $updateContent = $("<button>");
+      const $icon = $("<i>");
 
       const amPm = moment(`2000-01-01T09:00:00+0${i}:00`).format("A");
-      const timeDiff = (amPm ? dayHours[i] : dayHours[i] + 12) - moment().format("HH");
-      const bcgColor = timeDiff < 0 ? "gray" : (timeDiff === 0 ? "blue" : "green");
+      const timeDiff =
+        (amPm ? dayHours[i] : dayHours[i] + 12) - moment().format("HH");
+      const bcgColor =
+        timeDiff < 0
+          ? "lightgray"
+          : timeDiff === 0
+          ? "lightcoral"
+          : "lightgreen";
 
-      tasks.forEach(function(task) {
+      tasks.forEach(function (task) {
         if (task.id == i) {
-            $task.val(task.task);
+          $task.val(task.task);
         }
-      })
+      });
 
       $row.addClass("row align-items-center");
 
-      $time.text(`${dayHours[i]}${amPm}`).addClass("col-2 align-items-center").css({
-        border: "2px solid black",
-        padding: "27px",
-      });
+      $time
+        .text(`${dayHours[i]}${amPm}`)
+        .addClass("col-2 align-items-center")
+        .css({
+          border: "2px solid black",
+          padding: "27px",
+          "background-color": "lightblue",
+          "font-weight": "bold",
+        });
 
       $task
         .text(" ")
@@ -48,50 +63,56 @@ $(document).ready(function () {
         .addClass("col-8 task")
         .attr("id", i);
 
-      $updateContent.addClass("col-2 d-inline-block").css("height", "84px").attr("id", i);
+      $icon.addClass("fa fa-save fa-2x");
+      $updateContent.addClass("col-2 d-inline-block").attr("id", i).css({
+        height: "82px",
+        "background-color": "lightblue",
+      });
 
-      $schedule.append($row.append($time, $task, $updateContent));
-
+      $schedule.append($row.append($time, $task, $updateContent.append($icon)));
     }
 
+    // Method to handle the click event for the save button
     function handleClick(e) {
-        const id = e.target.id;
-        const $task = $(`.task[id=${id}]`);
-        let textToUpdate;
+      const id = e.target.id;
+      const $task = $(`.task[id=${id}]`);
+      let textToUpdate;
 
-        for (let i = 0; i < $task.length; i++) {
-            if ($task[i].id === id) {
-                textToUpdate = $task.val();
-            }
+      for (let i = 0; i < $task.length; i++) {
+        if ($task[i].id === id) {
+          textToUpdate = $task.val();
         }
-        updateStorage(textToUpdate, id)
+      }
+      updateStorage(textToUpdate, id);
     }
 
+    // Method to update the local storage of tasks
     function updateStorage(text, index) {
-        for (let i = 0; i < tasks.length; i++) {
-            if (tasks[i].id === index) {
-                tasks[i].task = text;
-                console.log(tasks)
-            } else {
-                tasks.push({
-                    id: index,
-                    task: text,
-                });
-                console.log(tasks)
-            }
+      for (let i = 0; i < tasks.length; i++) {
+        if (tasks[i].id === index) {
+          tasks[i].task = text;
+          console.log(tasks);
+          break;
+        } else {
+          tasks.push({
+            id: index,
+            task: text,
+          });
+          console.log(tasks);
+          break;
         }
-        if (tasks.length === 0) {
-            tasks.push({
-                id: index,
-                task: text,
-            });
-            console.log(tasks)
-        }
-        localStorage.setItem("tasks", JSON.stringify(tasks));
+      }
+      if (tasks.length === 0) {
+        tasks.push({
+          id: index,
+          task: text,
+        });
+        console.log(tasks);
+      }
+      localStorage.setItem("tasks", JSON.stringify(tasks));
     }
 
-    $(document).on("click", "button", handleClick)
-
-
+    // Event delegation to spot button clicks
+    $(document).on("click", "button", handleClick);
   }
 });
